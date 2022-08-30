accf40ec15b13e70771e5ae3341a1b7cd0e3eb6b
danie <danielgeorge3344@gmail.com>
2022-08-30 15:14:29 -0400

    Update file to include today's changes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,11 +3,21 @@
 
 public class HelloWorld {
 
+    /*
+        This is a multi line block for comments
+        as many lines as you need
+     */
+
     public static void main(String [] args){
         System.out.println("Hello world");
 
         int x = 5;
         boolean z = true;
         double pi = 3.14159;
+        String name = "CS219 Techniques of Computer Science";
+
+        double avagadros = 6.022140857e23;
+        double electron_mass = 9.1093837015e-31;
+
     }
 }
