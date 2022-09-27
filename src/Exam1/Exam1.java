32c7985c61dfd5ea68d8a9657092b78cea358481
danie <danielgeorge3344@gmail.com>
2022-09-27 15:29:24 -0400

    Use packages and new examples

---
+++ src/Exam1/Exam1.java
@@ -0,0 +1,51 @@
+package Exam1;
+
+import java.util.Scanner;
+
+public class Exam1 {
+
+    public static int numberOfDigits(int n){
+        int cnt = 0;
+        int m = 0;
+        while(n > 0){
+            m = n % 10;
+            if (m > 2 && m < 7){
+                cnt ++;
+            }
+            n = n / 10;
+        }
+        return cnt;
+    }
+
+    public static int validateInput(){
+        int n;
+
+        while (true) {
+            Scanner kbd = new Scanner(System.in);
+            System.out.print("Enter an integer: ");
+            if(kbd.hasNextInt()){
+                n = kbd.nextInt();
+                if (n < 0){
+                    n = n * -1;
+                }
+                break;
+            }
+            else if(kbd.hasNextDouble()){
+
+                System.out.printf("Be sure to enter an integer, you entered %f\n ", kbd.nextDouble());
+            }
+            else{
+                System.out.printf("Be sure to enter an integer, you entered %s\n ", kbd.next());
+            }
+
+        }
+        return n;
+    }
+
+
+    public static void main(String [] args) {
+        int n = validateInput();
+
+        System.out.print(numberOfDigits(n));
+    }
+}
